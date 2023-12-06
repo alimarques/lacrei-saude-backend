95a3205d2ad4240518838985dbbf3a01ebe95e4d
Ali Marques <ali.mrques@gmail.com>
2023-12-06 19:17:46 -0300

    Criar views de profissionais e consultas

--- lacreisaude/views.py
+++ lacreisaude/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Consulta, Profissional
+from .serializers import ConsultaSerializer, ProfissionalSerializer
+
+
+class ProfissionalViewSet(viewsets.ModelViewSet):
+    queryset = Profissional.objects.all()
+    serializer_class = ProfissionalSerializer
+    #permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+
+class ConsultaViewSet(viewsets.ModelViewSet):
+    queryset = Consulta.objects.all()
+    serializer_class = ConsultaSerializer
+    #permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['profissional']
