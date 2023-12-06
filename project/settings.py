483c236adab8093e5867260145b60a15e9822eaf
Ali Marques <ali.mrques@gmail.com>
2023-12-06 19:18:27 -0300

    Add apps no arquivo settings

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'lacreisaude',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
